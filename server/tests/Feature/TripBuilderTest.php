f96562829d32c92605b0ac3bf4862167e1ac8d35
ericellb <ericellb@gmail.com>
2019-11-07 01:22:00 -0500

    Tests for JSON Response for Trip Building

---
+++ server/tests/Feature/TripBuilderTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class TripBuilderTest extends TestCase
+{
+    /**
+     * Tests if we can request a one way trip by validating json structure
+     *
+     * @return void
+     */
+    public function test_a_user_can_request_a_oneway()
+    {
+        $this->withoutExceptionHandling();
+        $searchQuery = ['type' => 'oneway', 'seg0_from' => 'YUL', 'seg0_to' => 'YVR', 'seg0_date' => 'Nov 15 2019'];
+        $response = $this->json('GET', 'api/search', $searchQuery);
+        $this->assertOneWayJsonStructure($response);
+    }
+
+    /**
+     * Tests if we can request a round trip by validating json structure
+     *
+     * @return void
+     */
+    public function test_a_user_can_request_a_roundtrip()
+    {
+        $this->withoutExceptionHandling();
+        $searchQuery = ['type' => 'roundtrip',
+            'seg0_from' => 'YUL', 'seg0_to' => 'YVR', 'seg0_date' => 'Nov 15 2019',
+            'seg1_from' => 'YVR', 'seg1_to' => "YUL", 'seg1_date' => 'Nov 16 2019'];
+
+        $response = $this->json('GET', 'api/search', $searchQuery);
+        $this->assertTripJsonStructure($response);
+    }
+
+    /**
+     * Tests if we can request a round trip by validating json structure
+     *
+     * @return void
+     */
+    public function test_a_user_can_request_a_multi_trip()
+    {
+        $this->withoutExceptionHandling();
+        $searchQuery = ['type' => 'roundtrip',
+            'seg0_from' => 'YUL', 'seg0_to' => 'YVR', 'seg0_date' => 'Nov 15 2019',
+            'seg1_from' => 'YVR', 'seg1_to' => "YUL", 'seg1_date' => 'Nov 16 2019',
+            'seg2_from' => 'YUL', 'seg2_to' => "YVR", 'seg2_date' => 'Nov 17 2019'];
+
+        $response = $this->json('GET', 'api/search', $searchQuery);
+        $this->assertTripJsonStructure($response);
+    }
+
+    private function assertOneWayJsonStructure($response)
+    {
+        $response->assertJsonStructure([
+            'trips' => [
+                '*' => [
+                    'id',
+                    'departure_airport',
+                    'departure_time',
+                    'arrival_airport',
+                    'arrival_time',
+                    'airline_code',
+                    'airline_number',
+                    'price',
+                ],
+            ],
+        ]);
+    }
+
+    private function assertTripJsonStructure($response)
+    {
+        return $response->assertJsonStructure([
+            'trips' => [
+                '*' => [
+                    '*' => [
+                        'id',
+                        'departure_airport',
+                        'departure_time',
+                        'arrival_airport',
+                        'arrival_time',
+                        'airline_code',
+                        'airline_number',
+                        'price',
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+}
