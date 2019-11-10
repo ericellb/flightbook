1b57e18e4da14d6369b5258adf31226f3b0cf460
ericellb <ericellb@gmail.com>
2019-11-10 17:01:40 -0500

    Frontend deployment script + use prod api when client in prod

--- client/src/components/AxiosClient/index.ts
+++ client/src/components/AxiosClient/index.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8000"
-    : "https://lb.ltng.link";
+const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:8000" : "https://lb.ltng.link/flightbook";
 
 export default axios.create({
   baseURL: baseUrl
